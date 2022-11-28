eedce29375a955830aa0e0ddc0f438f2d344f45c
Brandon Krigbaum <brandonbk@gmail.com>
2022-11-28 16:38:05 -0600

    Adjust leaderboard ad sizes

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -23,16 +23,16 @@ module.exports = ({
     })
     .setTemplate('LEADERBOARD', {
       size: [
-        [970, 90],
+        [970, 250],
         [300, 100],
         [300, 250],
         [320, 50],
         [265, 149], // Featured Pushdown / Fluid Banner
       ],
       sizeMapping: [
-        { viewport: [980, 0], size: [[970, 90], [728, 90], [265, 149]] },
-        { viewport: [750, 0], size: [[265, 149]] },
-        { viewport: [300, 0], size: [[300, 100], [265, 149]] },
+        { viewport: [980, 0], size: [[970, 250], [265, 149]] },
+        { viewport: [750, 0], size: [[300, 50], [265, 149]] },
+        { viewport: [300, 0], size: [[300, 50], [320, 50], [265, 149]] },
       ],
     })
     .setTemplate('ROTATION', {
