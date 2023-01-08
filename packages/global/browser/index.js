b7ce09a13fd1e2535740454b10bf3930c9f1fd2a
Brandon Krigbaum <brandonbk@gmail.com>
2023-01-08 10:18:32 -0600

    Remove braze references

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,12 +2,10 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
-const PreferenceCenter = () => import(/* webpackChunkName: "braze-preference-center" */ './braze-preference-center.vue');
 
 export default (Browser) => {
   MonoRail(Browser, {
     enableOmedaIdentityX: false,
   });
   Browser.register('ContentMeterTrack', ContentMeterTrack);
-  Browser.register('PreferenceCenter', PreferenceCenter);
 };
